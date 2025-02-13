06e75da5ab878194e8e234558ccc23679c592c3c
Ahmed Gamal <ag8577634@gmail.com>
2025-02-13 13:17:34 +0200

    test

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hello_world():
 <html>
 <body>
 <center>
-<h1>Demo on GitOps with ArgoCD and Github Actions.</h1> <br>
+<h1>Demoo on GitOps with ArgoCD and Github Actions.</h1> <br>
 <be>
 <img src="https://github.com/jiMMY006007/sts-test-app/blob/main/itsworking.jpeg?raw=true">
 </center>
